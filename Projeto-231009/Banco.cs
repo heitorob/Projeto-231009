7c7d55d6e9d75a25b969faad2437220a8ea90648
Usuario <Usuario@CPD02-M03>
2024-09-24 12:29:27 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto-231009/Banco.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace Projeto_231009
+{
+    public class Banco
+    {
+        public static MySqlConnection Conectar;
+        public static MySqlCommand Comando;
+        public static MySqlDataAdapter Adaptar;
+        public static DataTable DataTabela;
+
+        public static void AbrirConexao()
+        {
+            try
+            {
+                Conectar = new MySqlConnection("server=localhost;port=3307;uid=root;pwd=etecjau");
+                Conectar.Open();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        public static void FecharConexao()
+        {
+            try
+            {
+                Conectar.Close();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Conectar.Close();
+            }
+        }
+
+        public static void CriarBanco()
+        {
+            try
+            {
+                AbrirConexao();
+                Comando = new MySqlCommand("CREATE DATABASE IF NOT EXISTS vendas; USE vendas", Conectar);
+                Comando.ExecuteNonQuery();
+
+                Comando = new MySqlCommand("CREATE TABLE IF NOT EXISTS cidades (" +
+                                            "id integer auto_increment primary key, " +
+                                            "nome char(40), " +
+                                            "uf char(2))", Conectar);
+                Comando.ExecuteNonQuery();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
